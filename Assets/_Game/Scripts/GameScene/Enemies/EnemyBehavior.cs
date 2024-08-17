855a94ede5de720f5af794fbe0f4fc9fb4fc4c8f
Filip Barva <barvafil@fel.cvut.cz>
2024-08-17 11:09:11 +0200

    BaseEnemy

---
+++ Assets/_Game/Scripts/GameScene/Enemies/EnemyBehavior.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using EasyButtons;
+
+[RequireComponent(typeof(EnemyMovement), typeof(Health), typeof(SpriteRenderer))]
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField] private EnemyInfo _infoTemplate;
+    
+    private Health _health;
+    private SpriteRenderer _renderer;
+    private EnemyMovement _movement;
+
+    private void Awake() {
+        _health = GetComponent<Health>();
+        _movement = GetComponent<EnemyMovement>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable() {
+        _movement.Speed = _infoTemplate.Speed;
+        _health.SetMaxHealth(_infoTemplate.Health);
+
+        _health.OnHealthChange.AddListener(ChangeHealthProgress);
+        //_health.OnDeath.AddListener();
+    }
+
+    private void OnDisable() {
+        _health.OnHealthChange.RemoveListener(ChangeHealthProgress);
+        //_health.OnDeath.AddListener();
+    }
+
+    public void ChangeHealthProgress(float damage) {
+        _renderer.material.SetFloat("_DamageProgress", damage/_health.MaxHealth);
+    }
+
+    public void SetTargetPoint(Vector3 target, UnityAction<EnemyBehavior> affterArrive = null) {
+        _movement.SetTargetPoint(target, () => {if(affterArrive != null) affterArrive(this);});
+    }
+
+    [Button]
+    public void HitEnemy(float dmg) {
+        _health.DealDamage(dmg);
+    }
+
+    [Button]
+    public void SetPoint(Vector3 target) {
+        _movement.SetTargetPoint(target);
+    }
+}
