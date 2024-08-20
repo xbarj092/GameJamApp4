4a549cecaca8fb9268e0797734e39b431de9349c
Jakub Bartušek <jack.bartusek@gmail.com>
2024-08-20 11:01:56 +0200

    projectile trail added

--- Assets/_Game/Scripts/GameScene/Towers/TowerTypes/ShootTower/Projectile.cs
+++ Assets/_Game/Scripts/GameScene/Towers/TowerTypes/ShootTower/Projectile.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -6,9 +5,11 @@ public class Projectile : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _rotateSpeed = 200f;
+    [SerializeField] private SpriteRenderer _renderer;
 
     private Transform _target;
     private float _damage;
+    private bool _dead;
 
     public void Init(float damage, GameObject target)
     {
@@ -16,6 +17,9 @@ public class Projectile : MonoBehaviour
         _target = target.transform;
         ObjectSpawner.Instance.ReturnObjectWithDelay(PoolType.Projectile, this, 50);
 
+        _rb.simulated = true;
+        _renderer.enabled = true;
+        _dead = false;
         Vector2 direction = (_target.position - transform.position).normalized;
         _rb.velocity = direction * _speed;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
@@ -36,6 +40,11 @@ public class Projectile : MonoBehaviour
     {
         if (_target == null)
         {
+            if (_dead)
+            {
+                return;
+            }
+
             _rb.velocity = transform.up * _speed;
             return;
         }
@@ -51,7 +60,11 @@ public class Projectile : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy") && collision.TryGetComponent(out Health health))
         {
             health.DealDamage(_damage);
-            ObjectSpawner.Instance.ReturnObject(PoolType.Projectile, this);
+            _rb.velocity = Vector2.zero;
+            _rb.simulated = false;
+            _renderer.enabled = false;
+            _dead = true;
+            ObjectSpawner.Instance.ReturnObjectWithDelay(PoolType.Projectile, this, 0.3f);
         }
     }
 }
